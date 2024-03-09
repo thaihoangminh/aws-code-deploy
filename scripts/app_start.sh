8496a1643028823395509afb7ddd5cb1f7881138
Thai Hoang Minh <thai.hoang@positivethinking.tech>
2024-03-09 15:40:20 +0700

    test aws codeDeploy

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/server/src
-npm start
-pm2 start npm --name "aws-code-deploy" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+
+systemctl -l enable nginx
+systemctl -l start nginx
